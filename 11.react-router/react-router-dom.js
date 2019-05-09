8603b1e5dacecacc0b11b83ae84a15559ad09982
Jenear <wuwj1126@126.com>
2019-05-09 21:23:16 +0800

    redux

---
+++ 11.react-router/react-router-dom.js
@@ -0,0 +1,11 @@
+/**
+ * 1:hash history ，有#号，不能重复点击同一个路由，否则会报错
+ * 
+ * 2:browser history,用的H5的一个特性
+ * 
+ * 3：运用的时候可以用HashHistory 和BrowseHistory  还有Route还有Link
+ * Link是点击跳转的标签
+ * 下面是用来展示的部分
+ * HashHistory 和BrowseHistory是包括容器
+ * Route是一条一条的路由
+ */
---
+++ 7.redux/2.redux使用步骤.js
@@ -0,0 +1,6 @@
+// 一般使用react-redux的步骤大致如下
+//  创建常量（action types）--> 
+// 
+/**
+ * 创建actiontypes常量 --> 创建action --> 创建reducer --> 合并reducers --> 创建store --> 用Provider标签去包括根目录，并且把store存入
+ */
